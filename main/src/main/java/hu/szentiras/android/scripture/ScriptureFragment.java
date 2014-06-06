11fd0a124b9a9974e89fe84e6e6629b7645f40ed
briff <fodber@organum.hu>
2014-06-06 15:57:56 +0200

    Create menu for scripture fragment

--- main/src/main/java/hu/szentiras/android/scripture/ScriptureFragment.java
+++ main/src/main/java/hu/szentiras/android/scripture/ScriptureFragment.java
@@ -3,9 +3,8 @@ package hu.szentiras.android.scripture;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
+import android.support.v4.view.MenuItemCompat;
+import android.view.*;
 import hu.szentiras.android.MainActivity;
 import hu.szentiras.android.R;
 
@@ -14,6 +13,11 @@ import hu.szentiras.android.R;
  */
 public class ScriptureFragment extends Fragment {
 
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -21,8 +25,22 @@ public class ScriptureFragment extends Fragment {
     }
 
     @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+    }
+
+    @Override
     public void onAttach(Activity activity) {
         super.onAttach(activity);
         ((MainActivity) activity).onSectionAttached(2);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        MenuItem item = menu.add(Menu.NONE, Menu.NONE, Menu.NONE, "K");
+        MenuItemCompat.setShowAsAction(item, MenuItemCompat.SHOW_AS_ACTION_IF_ROOM);
+
+        inflater.inflate(R.menu.global, menu);
     }
 }
